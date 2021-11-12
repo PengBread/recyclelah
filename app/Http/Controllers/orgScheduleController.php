52c8f242cfe0ad646f025be25bf3dd0683bad61e
PengBread <pengbreadpersonal@gmail.com>
2021-11-12 19:48:28 +0800

    .

--- app/Http/Controllers/orgScheduleController.php
+++ app/Http/Controllers/orgScheduleController.php
@@ -67,7 +67,7 @@ class orgScheduleController extends Controller
             'scheduleName' => 'required|string|max:50',
             'scheduleDateStart' => 'required|date',
             'scheduleDateEnd' => 'required|date',
-            'scheduleContent'=>'required|string|max:500'
+            'scheduleContent'=>'required|string|max:600'
         ]);
 
         $stateSelection = $request->get('stateName');
